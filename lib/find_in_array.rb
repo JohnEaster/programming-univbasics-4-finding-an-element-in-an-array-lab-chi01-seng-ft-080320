4c17d6a5d19eff43f5d0f087d6b408eaebdb73ca
John Easter <john@owltestprep.com>
2020-07-11 13:37:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,11 @@
 def find_element_index(array = [1, 2, 3, 4, 5, 6], value_to_find = 7)
   counter = 0
+  found_value_index = nil
   while counter < array.length do
-    puts array[counter] == value_to_find
+    if array[counter] == value_to_find
+      found_value_index = counter
+    end
     counter += 1
   end
+  found_value_index
 end
